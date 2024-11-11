e3567c799cd4bc087e115035b2b0a933c446b310
Ted Palmer <etpalmer17@gmail.com>
2024-11-11 17:22:34 -0500

    Update og image

--- apps/web/src/pages/metatags.ts
+++ apps/web/src/pages/metatags.ts
@@ -6,8 +6,8 @@ import { t } from 'uniswap/src/i18n'
 const DEFAULT_METATAGS: MetaTagInjectorInput = {
   title: t('interface.metatags.title'),
   description: t`interface.metatags.description`,
-  image: `https://app.uniswap.com/images/1200x630_Rich_Link_Preview_Image.png`,
-  url: 'https://app.uniswap.com',
+  image: `https://reservoir.app/images/1200x630_Rich_Link_Preview_Image.png`,
+  url: 'https://reservoir.app',
 }
 
 type MetatagAttributes = { property?: string; name?: string; content: string }
