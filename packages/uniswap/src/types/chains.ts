f14af8eb9bc321b5576a875623a41be5743a8552
Ted Palmer <etpalmer17@gmail.com>
2024-11-11 21:44:45 -0500

    Add back base and mainnet

--- packages/uniswap/src/types/chains.ts
+++ packages/uniswap/src/types/chains.ts
@@ -64,7 +64,7 @@ export const WALLET_SUPPORTED_CHAIN_IDS: WalletChainId[] = [
 export type InterfaceChainId = UniverseChainId
 
 export const WEB_SUPPORTED_CHAIN_IDS: InterfaceChainId[] = [
-  // UniverseChainId.Mainnet,
+  UniverseChainId.Mainnet,
   // UniverseChainId.Goerli,
   // UniverseChainId.Sepolia,
   // UniverseChainId.Optimism,
@@ -77,7 +77,7 @@ export const WEB_SUPPORTED_CHAIN_IDS: InterfaceChainId[] = [
   // UniverseChainId.Celo,
   // UniverseChainId.CeloAlfajores,
   // UniverseChainId.Bnb,
-  // UniverseChainId.Base,
+  UniverseChainId.Base,
   // UniverseChainId.Blast,
   // UniverseChainId.Zora,
   // UniverseChainId.Zksync,
